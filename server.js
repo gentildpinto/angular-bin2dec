28aef69fef1732e48e988436a5b7ef39af99fddb
GentilPinto <gentil.junior.gp@gmail.com>
2020-05-17 03:14:36 +0100

    fix some bugs again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+    res.status(200).sendFile(__dirname + '/dist/bin2dec/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
