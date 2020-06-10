49a2866fd9745f7590aa9596d32d599258ae0c24
GentilPinto <gentil.junior.gp@gmail.com>
2020-06-10 21:49:12 +0100

    preparing for production enviroment

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/bin2dec/index.html');
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Connected to Port ${8080}`); //Listening on port 4201
+});
